6b3996152767af60d40c0249d84f362d774ec83d
Hussein <hussein@knoesis.org>
2017-03-30 16:22:07 -0400

    final evaluations"

--- Evaluation/main.py
+++ Evaluation/main.py
@@ -88,98 +88,115 @@ def init_using_elasticindex(bb):
 
     lnex.elasticindex(conn_string='130.108.85.186:9200', index_name="photon_v1")
 
-    return lnex.initialize(bb, augment=True)
+    return lnex.initialize(bb, augment=False)
 
 ################################################################################
 
 if __name__ == "__main__":
 
-    # chennai flood bounding box
-    chennai_bb = [12.74, 80.066986084, 13.2823848224, 80.3464508057]
+    bbs = {
+        "chennai": [12.74, 80.066986084, 13.2823848224, 80.3464508057],
+        "louisiana": [29.4563,-93.3453,31.4521,-89.5276],
+        "houston": [29.4778611958,-95.975189209,30.1463147381,-94.8889160156]
+    }
 
-    #init_using_elasticindex(chennai_bb)
+    for bb in bbs:
 
-    init_using_files()
+        if bb != "chennai":
+            continue
 
-    ############################################################################
+        init_using_elasticindex(bbs[bb])
 
-    filename = "Chennai_annotations.json"
+        ########################################################################
 
-    anns = read_annotations(filename)
+        filename = bb+"_annotations.json"
 
-    TPs_count = .0
-    FPs_count = .0
-    FNs_count = .0
-    overlaps_count = 0
+        anns = read_annotations(filename)
 
-    for key in anns:
+        TPs_count = .0
+        FPs_count = .0
+        FNs_count = .0
+        overlaps_count = 0
 
-        tweet_lns = set()
-        lnex_lns = set()
-        tweet_text = ""
+        for key in anns:
 
-        for ann in anns[key]:
-            if ann != "text":
-                ln = anns[key][ann]
+            tweet_lns = set()
+            lnex_lns = set()
+            tweet_text = ""
 
-                tweet_lns.add(((int(ln['start_idx']), int(ln['end_idx'])),
-                                    ln['type']))
-            else:
-                tweet_text = anns[key][ann]
-                #print tweet_text
-                lnex_lns = set([x[1] for x in lnex.extract(tweet_text)])
+            for ann in anns[key]:
+                if ann != "text":
+                    ln = anns[key][ann]
 
-        #print tweet_lns, [tweet_text[x[0]:x[1]] for x in tweet_lns]
-        #print lnex_lns, [tweet_text[x[0]:x[1]] for x in lnex_lns]
+                    tweet_lns.add(((int(ln['start_idx']), int(ln['end_idx'])),
+                                        ln['type']))
+                else:
+                    tweet_text = anns[key][ann]
+                    #print tweet_text
+                    lnex_lns = set([x[1] for x in lnex.extract(tweet_text)])
 
-        # The location names of type outLoc and ambLoc that the tools should've
-        #   not extracted them
-        tweet_lns_not_inLocs = set([x[0] for x in tweet_lns if x[1] != 'inLoc'])
+            #print tweet_lns, [tweet_text[x[0]:x[1]] for x in tweet_lns]
+            #print lnex_lns, [tweet_text[x[0]:x[1]] for x in lnex_lns]
 
-        # remove lns that are not inLoc as we already calculated their effect+++
-        tweet_lns = set([x[0] for x in tweet_lns]) - tweet_lns_not_inLocs
+            # The location names of type outLoc and ambLoc that the tools
+            #   should've not extracted them
+            tweet_lns_not_inLocs = set([x[0] for x in tweet_lns
+                                            if x[1] != 'inLoc'])
+            # add all extracted lns than are of types other than inLoc as FPs
+            FPs_count += len(lnex_lns & tweet_lns_not_inLocs)
 
-        # True Positives +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        TPs = tweet_lns & lnex_lns
+            # remove LNs that are not inLoc, we already calculated their effect
+            tweet_lns = set([x[0] for x in tweet_lns]) - tweet_lns_not_inLocs
+            lnex_lns -= tweet_lns_not_inLocs
 
-        TPs_count += len(TPs)
+            # True Positives +++++++++++++++++++++++++++++++++++++++++++++++++++
+            TPs = tweet_lns & lnex_lns
 
-        # Left in both sets ++++++++++++++++++++++++++++++++++++++++++++++++++++
-        tweet_lns = tweet_lns - TPs
-        lnex_lns = lnex_lns - TPs
+            TPs_count += len(TPs)
 
-        # Find Overlapping Location Names to be counted as 1/2 FPs and 1/2 FNs++
-        overlaps = set()
-        for x in tweet_lns:
-            for y in lnex_lns:
-                if do_they_overlap(x, y):
-                    overlaps.add(x)
-                    overlaps.add(y)
+            # Left in both sets ++++++++++++++++++++++++++++++++++++++++++++++++
+            tweet_lns -= TPs
+            lnex_lns -= TPs
 
-        # add all non-inLoc even if only overlaps as FPs
-        FPs_count += len(lnex_lns & tweet_lns_not_inLocs) + \
-                     len(overlaps & tweet_lns_not_inLocs)
+            # Find Overlapping LNs to be counted as 1/2 FPs and 1/2 FNs++
+            overlaps = set()
+            for x in tweet_lns:
+                for y in lnex_lns:
+                    if do_they_overlap(x, y):
+                        overlaps.add(x)
+                        overlaps.add(y)
 
-        # count the number of overlaps that are of type inLoc as 1/2s
-        overlaps = overlaps - tweet_lns_not_inLocs
-        overlaps_count += len(overlaps)
+            # count all overlapping extractions with non-inLoc as FPs
+            FPs_count += len(overlaps & tweet_lns_not_inLocs)
 
-        # remove the overlapping lns and of type not inLoc from lnex_lns
-        lnex_lns = lnex_lns - (tweet_lns_not_inLocs | overlaps)
-        # before this line. tweet_lns contains all but the TPs and !inLoc
-        tweet_lns = tweet_lns - overlaps
+            # count the number of overlaps that are of type inLoc as 1/2s
+            overlaps = overlaps - tweet_lns_not_inLocs
+            overlaps_count += len(overlaps)
 
-        # False Positives ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        # lnex_lns = all - (TPs and overlaps and !inLoc)
-        FPs = lnex_lns - tweet_lns
-        FPs_count += len(FPs)
+            # remove the overlapping lns from lnex_lns and tweet_lns
+            lnex_lns -= overlaps
+            tweet_lns -= overlaps
 
-        # False Negatives ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        FNs = tweet_lns - lnex_lns
-        FNs_count += len(FNs)
+            # False Positives ++++++++++++++++++++++++++++++++++++++++++++++++++
+            # lnex_lns = all - (TPs and overlaps and !inLoc)
+            FPs = lnex_lns - tweet_lns
+            FPs_count += len(FPs)
 
-    Precision = TPs_count/(TPs_count + FPs_count + .5 * overlaps_count)
-    Recall = TPs_count/(TPs_count + FNs_count + .5 * overlaps_count)
+            # False Negatives ++++++++++++++++++++++++++++++++++++++++++++++++++
+            FNs = tweet_lns - lnex_lns
+            FNs_count += len(FNs)
 
-    print Precision
-    print Recall
+        '''
+        since we add 2 lns one from lnex_lns and one from tweet_lns if they
+        overlap the equation of counting those as 1/2 FPs and 1/2 FNs is going
+        to be:
+            overlaps_count x
+                1/2 (since we count twice) x
+                    1/2 (since we want 1/2 of all the errors made)
+
+        '''
+        Precision = TPs_count/(TPs_count + FPs_count + .5 * .5 * overlaps_count)
+        Recall = TPs_count/(TPs_count + FNs_count + .5 * .5 * overlaps_count)
+        F_Score = (2 * Precision * Recall)/(Precision + Recall)
+
+        print bb, Precision, Recall, F_Score
